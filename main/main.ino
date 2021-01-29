6957b5ecb2362ee9a9486b686ef9cf8b00bccfab
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2021-01-28 21:19:52 -0600

    Added UI. Needs tested

--- main/main.ino
+++ main/main.ino
@@ -11,12 +11,19 @@ unsigned long currentReadCopy;
 unsigned long computeFlagCopy;
 IntervalTimer interruptTimer;
 
+
 int phaseDetect[4] = {0, 0, 0, 0};            //keeps track of what phase each filter is at
 double amplitudes[4] = {0.0, 0.0, 0.0, 0.0};  //keeps track of what amplitude each filter is at
 
 #define ADC_PIN A7
 #define SAMPLING_RATE 500   //for 2kHz or 0.0005 s
-#define thresh 100 //Remember, the max is 514 here!
+#define default_thresh 100 //Remember, the max is 514 here!
+
+String entry;
+int triggering_faze = 0;
+int global_thresh = 100;
+int currentbest = 0;
+bool UI_done_flag = false;
 
 unsigned current_bank = 0;
 unsigned alt_bank_counter = 0;
@@ -28,16 +35,18 @@ unsigned alt_bank_counter = 0;
 
 void readADC(void);
 int getMaxAmplitude(double* amplitudes, unsigned* current, unsigned* counter);
+void welcome();
+void user_interface();
 
 //===========
 // Buffers
 //===========
 
 //initializes filters
-jd::buffer band_20_50(a_20_50_2, b_20_50_2, 2, a_20_50_4, b_20_50_4, 4, thresh, &phaseDetect[0]);
-jd::buffer band_40_70(a_40_70_2, b_40_70_2, 2, a_40_70_4, b_40_70_4, 4, thresh, &phaseDetect[1]);
-jd::buffer band_60_90(a_60_90_2, b_60_90_2, 2, a_60_90_4, b_60_90_4, 4, thresh, &phaseDetect[2]);
-jd::buffer band_80_110(a_80_110_2, b_80_110_2, 2, a_80_110_4, b_80_110_4, 4, thresh, &phaseDetect[3]);
+jd::buffer band_20_50(a_20_50_2, b_20_50_2, 2, a_20_50_4, b_20_50_4, 4, default_thresh, &phaseDetect[0]);
+jd::buffer band_40_70(a_40_70_2, b_40_70_2, 2, a_40_70_4, b_40_70_4, 4, default_thresh, &phaseDetect[1]);
+jd::buffer band_60_90(a_60_90_2, b_60_90_2, 2, a_60_90_4, b_60_90_4, 4, default_thresh, &phaseDetect[2]);
+jd::buffer band_80_110(a_80_110_2, b_80_110_2, 2, a_80_110_4, b_80_110_4, 4, default_thresh, &phaseDetect[3]);
 
 //===========
 // Setup
@@ -55,31 +64,42 @@ void setup() {
 // Main Loop
 //==========
 
-int i = 0;
-int currentbest = 0;
 void loop() {
-  //gets ADC (or read from vector at 2 kHz
-  noInterrupts();
-  computeFlagCopy = computeFlag;
-  currentReadCopy = currentRead;
-  interrupts();
 
-  //if we read a new value: do stuff. else: dont do stuff
-  if (computeFlagCopy == 1) {
+  //===============
+  // User Interface
+  //===============
+  if (Serial.available() && UI_done_flag == false) {
+    user_interface();
+  } else {
+   
+    //====================================================
+    // Signal processing. Occurs when no serial plugged in
+    //====================================================
 
-    //adds new read to each band and updates the amplitude readings
-    amplitudes[0] = band_20_50.insert((double)currentReadCopy);
-    amplitudes[1] = band_40_70.insert((double)currentReadCopy);
-    amplitudes[2] = band_60_90.insert((double)currentReadCopy);
-    amplitudes[3] = band_80_110.insert((double)currentReadCopy);
-
-    //NOT AGAIN!!
+    //gets ADC (or read from vector at 2 kHz
     noInterrupts();
-    computeFlag = 0;
+    computeFlagCopy = computeFlag;
+    currentReadCopy = currentRead;
     interrupts();
 
-    //ADD TRIGGERING
-    currentbest = getMaxAmplitude(amplitudes, &current_bank, &alt_bank_counter);
+    //if we read a new value: do stuff. else: dont do stuff
+    if (computeFlagCopy == 1) {
+
+      //adds new read to each band and updates the amplitude readings
+      amplitudes[0] = band_20_50.insert((double)currentReadCopy);
+      amplitudes[1] = band_40_70.insert((double)currentReadCopy);
+      amplitudes[2] = band_60_90.insert((double)currentReadCopy);
+      amplitudes[3] = band_80_110.insert((double)currentReadCopy);
+
+      //NOT AGAIN!!
+      noInterrupts();
+      computeFlag = 0;
+      interrupts();
+
+      //ADD TRIGGERING
+      currentbest = getMaxAmplitude(amplitudes, &current_bank, &alt_bank_counter);
+    }
   }
 }
 
@@ -117,4 +137,87 @@ int getMaxAmplitude(double* amplitudes, unsigned* current, unsigned* counter) {
 void readADC(void) {
   currentRead = analogRead(ADC_PIN);
   computeFlag = 1;
+}
+
+void welcome() {
+  Serial.println("===============");
+  Serial.println("Welcome to teensy interface for closed loop gamma modulation");
+  Serial.println();
+  Serial.println("Possible options:");
+  Serial.println("\tphase: adjust which phase you desire for triggering");
+  Serial.println("\t\t0 -> -/+ 0 cross");
+  Serial.println("\t\t1 -> peak,");
+  Serial.println("\t\t2 -> +/- 0 cross");
+  Serial.println("\t\t3 -> trough");
+  Serial.println("\tthreshold: determines the amplitude threshold required for triggering");
+  Serial.println("\t\t0-514 with 514 being +/- 3.3 V");
+  Serial.println("\tquit: exit this menu");
+  Serial.println("===============");
+}
+
+void user_interface() {
+
+  //uses global variables so could be a bit sketchy
+  entry = Serial.readStringUntil('\n');
+  welcome();
+  while (!entry.equals("quit") ) {
+    if (entry.equals("phase")) {
+      Serial.print("Phase is currently set at ");
+      Serial.println(triggering_faze);
+      Serial.print("Enter a new value: ");
+      while (1) {
+        if (Serial.available()) {
+          String new_val = Serial.readStringUntil('\n');
+          triggering_faze = new_val.toInt();
+          break;
+        }
+      }
+      Serial.println();
+      Serial.print("Phase set to ");
+      Serial.println(triggering_faze);
+      entry = String("job_complete");
+    } else if (entry.equals("threshold")) {
+      Serial.print("Threshold is currently set at ");
+      Serial.println(global_thresh);
+      Serial.print("Enter a new value: ");
+      while (1) {
+        if (Serial.available()) {
+          String new_val = Serial.readStringUntil('\n');
+          global_thresh = new_val.toInt();
+          break;
+
+          //NOTE: NEED TO ACTUALLY CHANGE THE THRESHOLD HERE
+        }
+      }
+      Serial.println();
+      Serial.print("Threshold set to ");
+      Serial.println(global_thresh);
+      entry = String("job_complete");
+    } else if (entry.equals("job_complete")) {
+      Serial.print("Enter a new option: ");
+      while (1) {
+        if (Serial.available()) {
+          entry = Serial.readStringUntil('\n');
+          break;
+        }
+      }
+    } else {
+      Serial.println("Incorrect entry. Try again");
+      Serial.println();
+      //while (!entry.equals("quit") || !entry.equals("phase") || entry.equals("threshold")) {
+      while (1) {
+        if (Serial.available()) {
+          entry = Serial.readStringUntil('\n');
+          break;
+        }
+      }
+    }
+  }
+  UI_done_flag = true;
+  band_20_50.restart((double)global_thresh);
+  band_40_70.restart((double)global_thresh);
+  band_60_90.restart((double)global_thresh);
+  band_80_110.restart((double)global_thresh);
+
+  Serial.println("Entering main code. Goodbye");
 }
