fb1ee1a7d243941f381e76cda452c00264006ed5
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2021-02-25 15:19:46 -0600

    Updated op amp

--- op-amp/calc_divider.m
+++ op-amp/calc_divider.m
@@ -3,7 +3,7 @@ function voltages = calc_divider(r)
 v_dc=5*r(3)/(r(2)+r(3));
 
 %% AC
-c=1e-7;
+c=1e-6;
 Xc=(sqrt(-1)*2*pi*60*c)^-1;
 v_ac_peak=abs(3.3*paradd(r(2),r(3))/(r(1)+Xc+paradd(r(2),r(3))));
 v_ac_trough=-3.3*abs(paradd(r(2),r(3))/(r(1)+Xc+paradd(r(2),r(3))));
@@ -13,6 +13,7 @@ voltages(1)=v_dc+v_ac_peak-3.3;   %-3.3 because we minimizing
 voltages(2)=v_dc+v_ac_trough-0.1; % for op-amp
  
 %% Some component constraints
-
+voltages(3)=r(1)-5100;
+voltages(4)=r(2)-15000;
 end
 
--- op-amp/calc_with_w.m
+++ op-amp/calc_with_w.m
@@ -8,7 +8,7 @@ v_ac_peak=abs(3.3.*paradd(r(2),r(3))./(r(1)+Xc+paradd(r(2),r(3))));
 v_ac_trough=-3.3.*abs(paradd(r(2),r(3))./(r(1)+Xc+paradd(r(2),r(3))));
 
 %% Add DC and AC
-voltages{1}=v_dc+v_ac_peak;   %-3.3 because we minimizing
+voltages{1}=v_dc+v_ac_peak;   
 voltages{2}=v_dc+v_ac_trough; % for op-amp
  
 end
